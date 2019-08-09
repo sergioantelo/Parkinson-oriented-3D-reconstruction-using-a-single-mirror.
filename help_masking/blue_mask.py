04d119e554137a273b33d8e39119bd9f72c91736
sergioantelo <santelocabello02@gmail.com>
2019-08-09 11:11:40 +0200

    Blue change

--- help_masking/blue_mask.py
+++ help_masking/blue_mask.py
@@ -3,29 +3,28 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
  
+#Introduce your image
 imagen = cv2.imread('images/Example/2_Blue_markers.png')
 hsv = cv2.cvtColor(imagen, cv2.COLOR_BGR2HSV)
 
-#Rango para color rojo
+#Red color range
 azul_bajos = np.array([100,65,75], dtype=np.uint8)
 azul_altos = np.array([130, 255, 255], dtype=np.uint8)
 
-#Máscaras con inRange()
+#Masking
 mask = cv2.inRange(hsv, azul_bajos, azul_altos)
-print("One")
 
-#Mostramos la mascara final y la imagen
+#Showing the final mask and the original image
 cv2.imshow('Filtered blue', mask)
-print("Two")
 cv2.imshow('Initial', imagen)
-print("Three")
+
+#Save the image in your address
 cv2.imwrite('images/Example/2_Mask_blue.png', mask)
 
-#Salir con ESC
+#Exit with esc key
 while(1):
     tecla = cv2.waitKey(5) & 0xFF
     if tecla == 27:
         break
  
 cv2.destroyAllWindows()
-print("Caramba")
